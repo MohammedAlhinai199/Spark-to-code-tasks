4383e8ec55bbeb846321c9128aced70dbc9d3823
MohammedAlhinai199 <c19malhinai@gmail.com>
2026-07-04 08:42:26 +0400

    Solve problem 6

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -84,6 +84,28 @@
                 }
 
             } while (guess != secretNumber);
+
+            //task 6 :
+            try
+            {
+                Console.Write("Enter first number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter second number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                int result = num1 / num2;
+                Console.WriteLine("Result: " + result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error: Cannot divide by zero.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Please enter valid whole numbers.");
+            }
+
         }
     }
 }
