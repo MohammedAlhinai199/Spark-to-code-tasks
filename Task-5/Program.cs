ad0cf187c25093affa0f134c6aed7ea03cb55abb
MohammedAlhinai199 <c19malhinai@gmail.com>
2026-07-09 10:05:31 +0400

    ADD project files

---
+++ Task-5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+
+            
+        }
+    }
+}
