f3b763452497bacb1342d5b26b881ec02bd769f9
MohammedAlhinai199 <c19malhinai@gmail.com>
2026-07-12 22:17:21 +0400

    Add project files

---
+++ mini project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace mini_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
